16ed1f3e8d5abf5a7f697bcf101761ce1e5e8ca8
jorgeali <jorgeali.ramirez23@gmail.com>
2017-11-19 21:56:30 -0600

    interfaces

---
+++ app/src/main/java/com/responsivas/myapplication/Inventario.java
@@ -0,0 +1,13 @@
+package com.responsivas.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inventario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+    }
+}
